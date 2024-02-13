ee27ca4ffdd4f1e19c7074a48e4cba3e2871b010
Leilin <105161004+Necentt@users.noreply.github.com>
2024-02-13 11:02:14 +0300

    added 3.2.2
    NN

--- 3 module/3.2.py
+++ 3 module/3.2.py
@@ -7,15 +7,18 @@ import matplotlib.pyplot as plt
 N = 20
 b = 3
 x1 = np.random.random(N)
-x2 = x1 + [np.random.randint(10)/10 for i in range(N)] + 0.1 + b
+x2 = x1 + [np.random.randint(10)/10 for i in range(N)] + b
 C1 = [x1, x2]
 x1 = np.random.random(N)
 x2 = x1 - [np.random.randint(10)/10 for i in range(N)] - 0.1 + b
 C2 = [x1, x2]
 f = [0 + b, 1 + b]
-w = np.array([1, -1])
+w1 = -0.5
+w2 = 0.5
+w3 = -b * w2
+w = np.array([w1, w2, w3])
 for i in range(N):
-    x = np.array([C2[0][i], C2[1][i]])
+    x = np.array([C2[0][i], C2[1][i], 1])
     y = np.dot(w, x)
     if y >= 0: print('Класс C1')
     else: print('Класс C2')
